86edb3752c4e559a358419f1f79ea995cd416e2b
willyb22 <willyb4project@gmail.com>
2024-08-15 12:24:53 +0700

    add notebooks

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './index.css'; // Importing global CSS styles if any
+
+// Get the root element from index.html
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component inside the root element
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+  </React.StrictMode>
+);
